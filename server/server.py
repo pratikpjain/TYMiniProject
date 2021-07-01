2720519a4c6be1a9485d453f59096a1f4094a882
Pratik Jain <pratikpjain.15@gmail.com>
2021-07-01 18:31:34 +0530

    changes are added

---
+++ server/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import util
+import pandas as pd
+# import seaborn as sns
+# df = pd.read_csv("bhp.csv")
+# sns.set(style="ticks", color_codes=True)
+# fig = sns.pairplot(df)
+# fig.savefig('my_plot.png')
+
+app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
